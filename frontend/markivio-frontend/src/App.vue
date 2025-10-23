a624ab00a46eb2cbb0660cf36f0dbbf50f823da7
damien <damien.venant@outlook.com>
2025-10-23 16:39:08 +0200

    feat: add auth

--- frontend/markivio-frontend/src/App.vue
+++ frontend/markivio-frontend/src/App.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <button @click="login">Log in</button>
+    <button @click="showToken" v-if="isAuthentificated">Show Token In Console</button>
+    <span>{{user}}</span>
+  </div>
 </template>
+<script lang="ts">
+  import { useAuth0 } from '@auth0/auth0-vue';
 
-<style scoped></style>
+  export default {
+    setup() {
+      const { loginWithRedirect, isAuthenticated, getAccessTokenSilently, user } = useAuth0();
+
+      return {
+        login: () => {
+          loginWithRedirect();
+        },
+        showToken: async () => {
+         const result = await getAccessTokenSilently();
+         console.log(result);
+        },
+        isAuthentificated: isAuthenticated,
+        user: user,
+      };
+    }
+  };
+</script>
--- frontend/markivio-frontend/src/main.ts
+++ frontend/markivio-frontend/src/main.ts
@@ -1,9 +1,27 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createAuth0 } from '@auth0/auth0-vue';
+import App from "./App.vue"
 import router from './router'
 
 const app = createApp(App)
 
+const audience: string = import.meta.env.VITE_MARKIVIO_AUTH_AUDIENCE;
+const domain: string = import.meta.env.VITE_MARKIVIO_AUTH_DOMAIN;
+const clientId: string = import.meta.env.VITE_MARKIVIO_AUTH_CLIENT_ID;
+
+console.log(audience);
+
 app.use(router)
+app.use(
+  createAuth0({
+    domain: domain,
+    clientId: clientId,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+      audience: audience,
+    },
+    useRefreshTokens: true,
+  })
+);
 
 app.mount('#app')
