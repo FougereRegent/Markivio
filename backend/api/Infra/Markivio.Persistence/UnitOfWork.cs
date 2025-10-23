86b77312f0852efd2b64c7c4aa649925dbc5f03f
damien <damien.venant@outlook.com>
2025-10-23 10:49:15 +0200

    feat: implements unit of works pattern

---
+++ backend/api/Infra/Markivio.Persistence/UnitOfWork.cs
@@ -0,0 +1,40 @@
+using Markivio.Persistence.Config;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Markivio.Persistence;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task BeginTransactionAsync(CancellationToken token = default);
+    Task SaveChangesAsync();
+    Task RollbackChangesAsync();
+}
+
+public class UnitOfWork(MarkivioContext dbcontext) : IUnitOfWork
+{
+    private IDbContextTransaction transaction = null!;
+
+    public async Task BeginTransactionAsync(CancellationToken token = default)
+    {
+        transaction = await dbcontext.Database.BeginTransactionAsync(token);
+    }
+
+    public void Dispose()
+    {
+        transaction.Dispose();
+    }
+
+    public async Task RollbackChangesAsync()
+    {
+        if (transaction is null)
+            throw new InvalidOperationException("You cannot rollback a transaction when a transaction has not opened");
+        await transaction.RollbackAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        if (transaction is null)
+            throw new InvalidOperationException("You cannot rollback a transaction when a transaction has not opened");
+        await transaction.CommitAsync();
+    }
+}
