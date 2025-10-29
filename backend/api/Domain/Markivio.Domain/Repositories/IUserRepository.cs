6be54a72a07ac687e198b20fd18f828e71dc56a0
damien <damien.venant@outlook.com>
2025-10-29 22:13:58 +0100

    feat: change repository

--- backend/api/Domain/Markivio.Domain/Repositories/IUserRepository.cs
+++ backend/api/Domain/Markivio.Domain/Repositories/IUserRepository.cs
@@ -4,6 +4,6 @@ namespace Markivio.Domain.Repositories;
 
 public interface IUserRepository : IGenericRepository<User>
 {
-    ValueTask<User> GetUserByAuthId(string authId, CancellationToken token = default);
+    ValueTask<User?> GetUserByAuthId(string authId, CancellationToken token = default);
     ValueTask<User?> GetUserInfoByToken(string JwtToken, CancellationToken token = default);
 }
