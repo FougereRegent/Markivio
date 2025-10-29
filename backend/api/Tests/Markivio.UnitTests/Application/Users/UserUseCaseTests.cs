07051522c99f1a67aa656bc7a78b72214177a694
damien <damien.venant@outlook.com>
2025-10-29 23:51:19 +0100

    test: add tests on Me

--- backend/api/Tests/Markivio.UnitTests/Application/Users/UserUseCaseTests.cs
+++ backend/api/Tests/Markivio.UnitTests/Application/Users/UserUseCaseTests.cs
@@ -1,3 +1,5 @@
+using FluentResults;
+using Markivio.Application.Dto;
 using Markivio.Application.Users;
 using Markivio.Domain.Entities;
 using Markivio.Domain.Repositories;
@@ -25,7 +27,7 @@ public class UserUseCaseTests
           .Returns(ValueTask.FromResult<User?>(null));
 
         //Act
-        FluentResults.Result result = await useCase.CreateNewUserOnConnection(new Markivio.Application.Dto.UserConnectionDto(""));
+        Result result = await useCase.CreateNewUserOnConnection(new Markivio.Application.Dto.UserConnectionDto(""));
 
         //Assert
         result.IsFailed.ShouldBeTrue();
@@ -43,7 +45,7 @@ public class UserUseCaseTests
         userRepositoryMock.Setup(pre => pre.Save(It.IsAny<User>()));
 
         //Act
-        FluentResults.Result result = await useCase.CreateNewUserOnConnection(new Markivio.Application.Dto.UserConnectionDto(""));
+        Result result = await useCase.CreateNewUserOnConnection(new Markivio.Application.Dto.UserConnectionDto(""));
 
         //Assert
         userRepositoryMock.Verify(pre => pre.Save(It.IsAny<User>()), Times.Never());
@@ -62,11 +64,42 @@ public class UserUseCaseTests
         userRepositoryMock.Setup(pre => pre.Save(It.IsAny<User>()));
 
         //Act
-        FluentResults.Result result = await useCase.CreateNewUserOnConnection(new Markivio.Application.Dto.UserConnectionDto(""));
+        Result result = await useCase.CreateNewUserOnConnection(new Markivio.Application.Dto.UserConnectionDto(""));
 
         //Assert
         userRepositoryMock.Verify(pre => pre.Save(It.IsAny<User>()), Times.Once());
         result.IsSuccess.ShouldBeTrue();
 
+    }
+
+    [Fact]
+    public async ValueTask Me_ShouldReturnFailedResult_WhenUserNotExist()
+    {
+        //Arrange
+        const string jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkpQZzA0RXJvajJGVEg1MmVURWJqTCJ9.eyJpc3MiOiJodHRwczovL21hcmtpdmlvLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExMjExMDYzMTEwNDcxNDAyMTg3NyIsImF1ZCI6WyJodHRwczovL2xvY2FsaG9zdDo4MDgwLyIsImh0dHBzOi8vbWFya2l2aW8uZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTc2MTU5OTIwNCwiZXhwIjoxNzYxNjg1NjA0LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXpwIjoiVFdsYlZBZ25Pem5FVVJHekZidkFydTE1aUdZMkNiQVcifQ.NJ9jSw0rKCNCVWvwl0No9WMf0cbbGyIvX_lC3LOus0l2-qtw2sY-xDwsISY5BnZhbyt-_0PA4PqwPdBoUaoJtdvbJG6LEKHMkwlYT9qo92cYLxFeY0w5Ev8aMjouuSAa5n2lDyK3Q6w18KZbRw6T_rTjRQu3ULriIemCTt-SQyJrmVaqb4RgQVgkrF09NTH3tSTgxVwFf0P-ey_PH7fVRWg3Bc-JJew6E6jJ1qWlQe358M1w_uCVdY5av9KY1rng3Ud-hokBnivID9tdf9z2eOt2cGyjoQ5lcaBi7U6jw-2l326ikDqrjQZm5wRtgWCAlzl9SUclq7V0QKGyCrw1gQ";
+        userRepositoryMock.Setup(pre => pre.GetUserByAuthId(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+          .Returns(ValueTask.FromResult<User?>(null));
+
+        //Act
+        Result<UserInformation> result = await useCase.Me(new Markivio.Application.Dto.UserConnectionDto(jwtToken));
+
+        //Assert
+        result.IsFailed.ShouldBeTrue();
+        result.Errors[0].Message.ShouldBe("User not found");
+    }
+
+    [Fact]
+    public async ValueTask Me_ShouldReturnUser()
+    {
+        //Arrange
+        const string jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkpQZzA0RXJvajJGVEg1MmVURWJqTCJ9.eyJpc3MiOiJodHRwczovL21hcmtpdmlvLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExMjExMDYzMTEwNDcxNDAyMTg3NyIsImF1ZCI6WyJodHRwczovL2xvY2FsaG9zdDo4MDgwLyIsImh0dHBzOi8vbWFya2l2aW8uZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTc2MTU5OTIwNCwiZXhwIjoxNzYxNjg1NjA0LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXpwIjoiVFdsYlZBZ25Pem5FVVJHekZidkFydTE1aUdZMkNiQVcifQ.NJ9jSw0rKCNCVWvwl0No9WMf0cbbGyIvX_lC3LOus0l2-qtw2sY-xDwsISY5BnZhbyt-_0PA4PqwPdBoUaoJtdvbJG6LEKHMkwlYT9qo92cYLxFeY0w5Ev8aMjouuSAa5n2lDyK3Q6w18KZbRw6T_rTjRQu3ULriIemCTt-SQyJrmVaqb4RgQVgkrF09NTH3tSTgxVwFf0P-ey_PH7fVRWg3Bc-JJew6E6jJ1qWlQe358M1w_uCVdY5av9KY1rng3Ud-hokBnivID9tdf9z2eOt2cGyjoQ5lcaBi7U6jw-2l326ikDqrjQZm5wRtgWCAlzl9SUclq7V0QKGyCrw1gQ";
+        userRepositoryMock.Setup(pre => pre.GetUserByAuthId(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+          .Returns(ValueTask.FromResult<User?>(new User()));
+
+        //Act
+        Result<UserInformation> result = await useCase.Me(new Markivio.Application.Dto.UserConnectionDto(jwtToken));
+
+        //Assert
+        result.IsSuccess.ShouldBeTrue();
     }
 }
