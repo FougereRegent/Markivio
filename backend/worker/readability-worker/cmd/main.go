9af8fb19b2ec2b4abc7f28f42da9d3a1d97ccf5a
damien <damien.venant@outlook.com>
2026-05-03 22:45:45 +0200

    test: add some tests

--- backend/worker/readability-worker/cmd/main.go
+++ backend/worker/readability-worker/cmd/main.go
@@ -5,15 +5,41 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/FougereRegent/Markivio/backend/worker/readability-worker/internal/infrastructure"
 )
+type EnvName string
+const (
+	PgUsernameEnv EnvName = "WORKER_PG_USERNAME"
+	PgPasswordEnv EnvName = "WORKER_PG_PASSWORD"
+	MqUserEnv EnvName = "WORKER_MQ_USER"
+	MqPasswordEnv EnvName = "WORKER_MQ_PASSWORD"
+)
+
+type Config struct {
+	PgUsername string
+	PgPassword string
+	MqUser string
+	MqPassword string
+}
+
+var config Config
+
+func init() {
+	config = Config{
+		PgUsername: os.Getenv(string(PgUsernameEnv)),
+		PgPassword: os.Getenv(string(PgPasswordEnv)),
+		MqUser: os.Getenv(string(MqUserEnv)),
+		MqPassword: os.Getenv(string(MqPasswordEnv)),
+	}
+}
 
 func main() {
 	testReadInfra := infrastructure.NewReadability(&http.Client{
 	})
 
-	result, err := testReadInfra.ConvertWebSiteToMarkdown("https://fr.wikipedia.org/wiki/Informatique")
+	result, err := testReadInfra.ConvertWebSiteToMarkdown(os.Args[1])
 
 	if err != nil {
 		log.Panicf("%s", err)
---
+++ backend/worker/readability-worker/internal/infrastructure/readability_test.go
@@ -0,0 +1,66 @@
+package infrastructure
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+)
+
+func TestCommonHeaders(t *testing.T) {
+	//Arrange
+	requiredHeader := commonHeaders
+
+	//Assert
+	snaps.MatchSnapshot(t, requiredHeader)
+}
+
+
+func TestReadability(t *testing.T) {
+	//Arrange
+	urls := []struct{
+		Name string
+		Url string
+	}{
+		{
+			Name: "Wikipedia_Informatique",
+			Url: "https://fr.wikipedia.org/wiki/Informatique",
+		},
+		{
+			Name: "Wikipedia_Math",
+			Url: "https://fr.wikipedia.org/wiki/Math_(logiciel)",
+		},
+		{
+			Name: "Golang_Sha256",
+			Url: "https://pkg.go.dev/crypto/sha256",
+		},
+		{
+			Name: "Testify",
+			Url: "https://github.com/stretchr/testify#installation",
+		},
+	}
+	w := WebSiteReadability{
+		client: &http.Client{},
+	}
+	//Act & Arrange
+	for _, url := range urls {
+		name := fmt.Sprintf("TestReadability_%s", url.Name)
+		t.Run(name, func(te *testing.T) {
+			hash := sha256.New()
+			result, err := w.ConvertWebSiteToMarkdown(url.Url)
+			if err != nil {
+				t.Fail()
+			}
+
+			if result == nil {
+				t.Fail()
+			}
+			b, _ := io.ReadAll(result)
+			hash.Write(b)
+			snaps.MatchSnapshot(t, fmt.Sprintf("%x", hash.Sum(nil)))
+		})
+	}
+}
