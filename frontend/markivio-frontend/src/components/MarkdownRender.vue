5f89bdcb001744a1cb87ae8d2bce2af0f84facc6
damien <damien.venant@outlook.com>
2026-06-07 13:42:56 +0200

    feat: change ui on my project

--- frontend/markivio-frontend/src/components/MarkdownRender.vue
+++ frontend/markivio-frontend/src/components/MarkdownRender.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-import markdownit from "markdown-it";
-import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css'; // Style de code (tu peux choisir un autre)
 import { computed } from "vue";
+import markdownit from "markdown-it";
+import markdownItAnchor from 'markdown-it-anchor'
+import markdownItAttrs from 'markdown-it-attrs'
+import markdownItContainer from 'markdown-it-container'
+import markdownItFootnote from 'markdown-it-footnote'
+import markdownItToc from 'markdown-it-toc-done-right'
+import hljs from 'highlight.js';
+import 'github-markdown-css/github-markdown.css'
+import 'highlight.js/styles/github-dark.css'
 
 const content = defineModel('content', { required: true, type: String });
 const md = markdownit({
@@ -12,19 +19,26 @@ const md = markdownit({
   typographer: false,
   highlight: function (str: string, lang: string): string {
 
-  if (lang && hljs.getLanguage(lang)) {
+    if (lang && hljs.getLanguage(lang)) {
       try {
         return '<pre><code class="hljs">' +
-               hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
-               '</code></pre>';
-      } catch (__) {}
+          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+          '</code></pre>';
+      } catch (__) { }
     }
 
     return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
   }
 });
 
+md.use(markdownItAnchor)
+  .use(markdownItAttrs)
+  .use(markdownItContainer)
+  .use(markdownItFootnote)
+  .use(markdownItToc)
+
 const renderMarkdown = computed(() => md.render(content.value));
+
 </script>
 
 <template>
