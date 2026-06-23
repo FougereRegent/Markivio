2a2cfc64154252cb6cb2d777dcd17fc9886966b7
damien <damien.venant@outlook.com>
2026-06-23 23:59:04 +0200

    feat: remake nav bar

--- frontend/markivio-frontend/src/components/NavBarComponent.vue
+++ frontend/markivio-frontend/src/components/NavBarComponent.vue
@@ -3,101 +3,110 @@ import { useI18n } from 'vue-i18n'
 import { ref } from 'vue';
 import { useVersionStore } from '@/stores/version-store';
 
-type itemStr = {
-  icon: string
-  label: string
-}
-
-type tagStr = {
-  label: string
-  color: string
-};
-
 const { t } = useI18n();
-const {version} = useVersionStore();
+const { version } = useVersionStore();
 
 
 const items = ref([
   {
-    separator: false
-  },
-  {
     items: [
       {
         label: "Tous les documents",
-        icon: "pi pi-align-justify",
+        icon: "ri-article-line",
         badge: 10,
       },
       {
         label: "Favoris",
-        icon: "pi pi-start",
+        icon: "ri-star-line",
         badge: 10,
       },
       {
+        label: "Récent",
+        icon: "ri-article-line",
+      },
+      {
         label: "A lire",
-        icon: "pi pi-book",
+        icon: "ri-book-line",
+        badge: 10,
+      },
+      {
+        label: "Archives",
+        icon: "ri-archive-line",
         badge: 10,
       },
     ]
   },
   {
     label: "Tags",
-    icon: "pi pi-book",
+    icon: "ri-bookmark-line",
     items: [
       {
         label: "React",
         color: "red",
-        bagde: 10
+        badge: 10
       },
       {
         label: "Vue JS",
         color: "blue",
-        bagde: 10
+        badge: 10
       },
       {
         label: "Design",
         color: "green",
-        bagde: 10
+        badge: 10
       },
       {
         label: "Javascript",
         color: "yellow",
-        bagde: 10
+        badge: 10
       },
       {
         label: "IA",
         color: "pink",
-        bagde: 10
+        badge: 10
       },
       {
         label: "Backend",
         color: "orange",
-        bagde: 10
+        badge: 10
       },
+      {
+        label: "Tous les tags",
+        icon: "ri-bookmark-line",
+      },
+      {
+        label: "Gérer les tags",
+        icon: "ri-settings-5-line",
+      }
     ]
+  },
+  {
+    separator: true,
   }
 ])
 </script>
 
 <template>
-  <Menu :model="items" class="w-full md:w-60">
+  <Menu :model="items" class="md:w-60 h-full">
     <template #submenulabel="{ item }">
-      <span class="text-primary font-bold" :class="item.icon"></span>
-      <span class="text-primary font-bold">{{ item.label }}</span>
+      <div class="flex flex-row justify-between w-full">
+        <span class="text-primary font-semibold text-2xl">{{ item.label }}</span>
+        <i class="text-gray-400 text-2xl ml-auto" :class="item.icon" />
+      </div>
     </template>
     <template #item="{ item, props }">
       <a v-ripple class="flex items-center" v-bind="props.action">
-        <div class="grid grid-flow-col grid-rows-1 gap-4 justify-center">
-          <span class="col-span-1 rounded-full w-4 h-4" v-if="item.color != undefined"
-          :style="{backgroundColor: item.color}"></span>
-          <span :class="item.icon" />
-          <span class="col-span-3">{{ item.label }}</span>
-          <span class="col-span-1">{{ item.badge }}</span>
+        <div class="flex flex-row gap-4 justify-start items-center w-full">
+          <span class="rounded-full w-4 h-4" v-if="item.color != undefined"
+            :style="{ backgroundColor: item.color }"></span>
+          <i class="text-gray-400 text-2xl" :class="item.icon" v-if="item.icon" />
+          <span class="text-xl">{{ item.label }}</span>
+          <span class="ml-auto">{{item.badge}}</span>
         </div>
       </a>
     </template>
-    <template #end>
-      <span class="inline-flex flex-col items-start">{{version}}</span>
+    <template #end class="mt-auto">
+      <span class="inline-flex flex-col items-start">{{ version }}</span>
     </template>
   </Menu>
 </template>
