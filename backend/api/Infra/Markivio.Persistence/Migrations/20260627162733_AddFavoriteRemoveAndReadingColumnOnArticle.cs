cfc44fd33cbd2bf122ee0e75eb82bec88c154a07
damien <damien.venant@outlook.com>
2026-06-27 18:27:52 +0200

    feat: add new columns

---
+++ backend/api/Infra/Markivio.Persistence/Migrations/20260627162733_AddFavoriteRemoveAndReadingColumnOnArticle.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Markivio.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFavoriteRemoveAndReadingColumnOnArticle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isFavorite",
+                table: "articles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isRemoved",
+                table: "articles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "reading",
+                table: "articles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isFavorite",
+                table: "articles");
+
+            migrationBuilder.DropColumn(
+                name: "isRemoved",
+                table: "articles");
+
+            migrationBuilder.DropColumn(
+                name: "reading",
+                table: "articles");
+        }
+    }
+}
